3c0df11aa4c44cab299361c2e41d97f64e68e89a
mohamedessam62001 <mazn9113@gmail.com>
2025-07-15 17:45:48 +0300

    ver 3

--- src/Pages/LoginPages/LoginPage.jsx
+++ src/Pages/LoginPages/LoginPage.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function LoginPage() {
   return (
-    <div className='w-full h-1/2 bg-amber-200 flex '>
-      
+    <div className='w-full h-full bg-gray-400  '>
+      <div className='w-full h-full flex flex-col py-1 gap-1 justify-center items-center '>
+      <input type="text" className='input input-accent' />
+      <input type="text" className='input input-accent' />
+      <button type="button" className='btn btn-primary'>Login</button>
+
+      </div>
     </div>
   )
 }
