6f4ebb37ad756b2ce2e57a97a169ef5621687ad4
mohamedessam62001 <mazn9113@gmail.com>
2025-07-15 17:36:36 +0300

    ver2

--- src/Pages/LoginPages/LoginPage.jsx
+++ src/Pages/LoginPages/LoginPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function LoginPage() {
   return (
-    <div className='w-full h-1/2 bg-amber-200'>
+    <div className='w-full h-1/2 bg-amber-200 flex '>
       
     </div>
   )
