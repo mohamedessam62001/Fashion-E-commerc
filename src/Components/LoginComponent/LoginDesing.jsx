b308a9c61c61b56b47556c2543fe32d042a85a51
mohamedessam62001 <mazn9113@gmail.com>
2025-07-17 01:01:45 +0300

    ver3

--- src/Components/LoginComponent/LoginDesing.jsx
+++ src/Components/LoginComponent/LoginDesing.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
+import React from "react";
+import  "./index.css";
+import img from "/src/Slacksimgs/slidesImg/Frame5.png";
 export default function LoginDesing() {
   return (
-      <div className='w-1/2 h-[80dvh] max-h-[100%] max-w-[100%] bg-amber-50'>
-      </div>
-  )
+    <div className="w-1/2 h-[80dvh] max-h-[100%] max-w-[100%] flex justify-center ">
+      <img id="img" src={img} className="object-cover" />
+    </div>
+  );
 }
--- src/Pages/LoginPages/LoginPage.jsx
+++ src/Pages/LoginPages/LoginPage.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Loginform from '../../Components/LoginComponent/Loginform'
-import LoginDesing from '../../Components/LoginComponent/LoginDesing'
+import React from "react";
+import Loginform from "../../Components/LoginComponent/Loginform";
+import LoginDesing from "../../Components/LoginComponent/LoginDesing";
 
 export default function LoginPage() {
   return (
-    <div className='w-full h-[80dvh] py- container mx-auto flex justify-center  items-center  bg-amber-100'>
-      <LoginDesing/>
-      <Loginform/>
+    <div
+      className={`
+          w-full 
+          h-[80dvh]
+          container
+          py-0.5
+          mx-auto flex
+          justify-center
+          items-center max-[100%]`}
+    >
+      {/* <LoginDesing /> */}
+      <Loginform />
     </div>
-    
-  )
+  );
 }
