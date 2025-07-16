241b8ce57470c8880739623dd03c9c7c9ba4b872
mohamedessam62001 <mazn9113@gmail.com>
2025-07-17 02:20:51 +0300

    ver4

--- src/Components/LoginComponent/Loginform.jsx
+++ src/Components/LoginComponent/Loginform.jsx
@@ -4,9 +4,9 @@ import React from "react";
 export default function Loginform() {
   const initialValues = { email: "", password: "" };
   return (
-    <div className="w-full  container h-[80dvh] max-h-[100%] max-w-[100%] flex flex-col items-center justify-center bg-[#ffffff] ">
+    <div className="w-full   container h-[80dvh]  flex flex-col items-center justify-center bg-[#ffffff] ">
       <Formik initialValues={initialValues}>
-        <Form className="flex flex-col  items-center justify-center w-full h-[50dvh] gap-[1rem] bg-[#ffffff] ">
+        <Form className="flex flex-col  items-center justify-center h-[80%] w-[40%] max-sm:h-[80%] container max-sm:w-[90%]  max-md:h-[80%]  max-md:w-[100%]  max-lg:h-[80%]  max-lg:w-[80%] gap-[1rem] bg-[#c2c2c2] rounded-2xl ">
     <h1 className="text-3xl text-black ">Good to see you again.</h1>
     <p className="text-[1rem] text-black">Please log in to your account.</p>
           <Field
@@ -18,7 +18,7 @@ export default function Loginform() {
           <Field
             type="password"
             name="password"
-            className="input text-[#4C4C4B] "
+            className="input input-neutral text-[#4C4C4B] "
             placeholder=" Enter You Password"
           />
           <div className="flex">
