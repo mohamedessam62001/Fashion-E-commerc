f608b832efecc1e94683ef218a887ab8b73c87cc
mohamedessam62001 <mazn9113@gmail.com>
2025-07-19 03:58:04 +0300

    ver10

--- src/Components/LoginComponent/Loginform.jsx
+++ src/Components/LoginComponent/Loginform.jsx
@@ -1,11 +1,29 @@
+import axios from "axios";
 import { Field, Form, Formik } from "formik";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Loginform() {
+  const Navigate = useNavigate()
   const initialValues = { email: "", password: "" };
+  const handleRegister = (values) => {
+    let domain = "http://localhost:1337";
+    let endPoint = "/api/auth/local";
+    let url = domain + endPoint;
+    axios.post(url,{
+      // username:"mohamed",
+       identifier:values.email,
+      password:values.password,
+    }).then((res)=>{
+      console.log(res.data);
+      Navigate("/")
+    })
+
+  };
+
   return (
-    <div className="w-full   container h-[80dvh]  flex flex-col items-center justify-center bg-[#ffffff] ">
-      <Formik initialValues={initialValues}>
+    <div className="w-full   container h-[80dvh]  flex flex-col items-center justify-center  ">
+      <Formik initialValues={initialValues} onSubmit={handleRegister}>
         <Form className="flex flex-col  items-center justify-center h-[80%] w-[40%] max-sm:h-[80%] container max-sm:w-[90%]  max-md:h-[80%]  max-md:w-[100%]  max-lg:h-[80%]  max-lg:w-[80%] gap-[1rem] bg-[#c2c2c2] rounded-2xl ">
           <h1 className="text-3xl text-black ">Good to see you again.</h1>
           <p className="text-[1rem] text-black">
@@ -24,7 +42,9 @@ export default function Loginform() {
             placeholder=" Enter You Password"
           />
           <div className="flex">
-            <button  className="btns text-black btn-neutral w-[200px]  ">Login</button>
+            <button type="Submit" className="btns text-black btn-neutral w-[200px]  ">
+              Login
+            </button>
           </div>
         </Form>
       </Formik>
---
+++ src/stor/index.jsx
@@ -0,0 +1,4 @@
+import axios from "axios"
+import { create } from "zustand"
+
+export const handleRegister= create((set)=>({}))
